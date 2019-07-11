1d2f3b90dab4219eba4b83a74846a2c9773c5ea6
Andrew Berndt <abrnst@gmail.com>
2019-07-10 21:57:10 -0500

    pushing

---
+++ views/js/login.js
@@ -0,0 +1,11 @@
+
+
+$("#submit").on("click", function() {
+
+    var username =$("#username").val();
+    var password=$("#password").val();
+
+    console.log(username);
+    console.log(password);
+
+});
